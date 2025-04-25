3c4eb91b26c2ccad7340346800db1b15c28eff65
seedevk8s <seedevk8s@gmail.com>
2025-04-25 17:36:12 +0900

    Counter컴포넌트추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 import './App.css'
 
+function Counter() {
+  return (
+    <button>Counter버튼</button>
+  )
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Counter />
       Hello Vite + React!
     </>
   )
