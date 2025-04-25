cfdfa9d2031918fc40a87136cc3b6c8071d70cc8
seedevk8s <seedevk8s@gmail.com>
2025-04-25 21:24:23 +0900

    1차 App 컴포넌트수정

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,36 @@ function App() {
 
   return (
     <>
-       <div>Hello 리엑트!</div>
-       <img src='/img/view.jpeg' alt='자연 이미지' width={500} height={200}/>
+      <main>
+        <div class="card">
+          <div class="card__header">강의 목록</div>
+          <div class="card__body">
+            <div class="courses">
+              <article class="course">
+                <img src='/img/view.jpeg' alt='자연 이미지' width={150} height={100}/>
+                <div class="course__body">
+                  <div class="course__title">입문자를 위한, HTML&CSS 웹 개발 입문</div>
+                  <div class="course__description">웹 개발에 필요한 기본 지식을 배웁니다. </div>
+                </div>
+              </article>
+              <article class="course">
+                <img src='/img/view.jpeg' alt='자연 이미지' width={150} height={100}/>
+                <div class="course__body">
+                  <div class="course__title">입문자를 위한, HTML&CSS 웹 개발 입문</div>
+                  <div class="course__description">웹 개발에 필요한 기본 지식을 배웁니다. </div>
+                </div>
+              </article>
+              <article class="course">
+                <img src='/img/view.jpeg' alt='자연 이미지' width={150} height={100}/>
+                <div class="course__body">
+                  <div class="course__title">입문자를 위한, HTML&CSS 웹 개발 입문</div>
+                  <div class="course__description">웹 개발에 필요한 기본 지식을 배웁니다. </div>
+                </div>
+              </article>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
   )
 }
