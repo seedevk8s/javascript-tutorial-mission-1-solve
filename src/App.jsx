9cd3667dbf647915f6875b5ad20213ccb755ea34
seedevk8s <seedevk8s@gmail.com>
2025-04-25 21:14:20 +0900

    Lab1 하기위한 준비

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,11 @@
 import './App.css'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Main from './components/Main'
-
-/* function Header() {
-  return (
-    <header>
-      <h1>Header헤더</h1>
-    </header>
-  )
-} */
-
-/* function Footer() {
-  return (
-    <footer>
-      <h1>Footer푸터</h1>
-    </footer>
-  )
-} */
-
-/* function Main() {
-  return (
-    <main>
-      <Counter />
-    </main>
-  )
-} */
-
-/* function Counter() {
-  return (
-    <button>Counter버튼</button>
-  )
-} */
 
 function App() {
 
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+       <div>Hello 리엑트!</div>
+       <img src='/img/view.jpeg' alt='자연 이미지' width={500} height={200}/>
     </>
   )
 }
